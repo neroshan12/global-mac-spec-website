b500e12a6d7273d263f02b0ac2e5904b12ad27e7
Nero <neroshan12@gmail.com>
2020-09-25 17:17:44 +0100

    Add image

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,11 +1,18 @@
-import React from 'react'
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
-
+import React from "react";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 
 const NavBar = () => {
   return (
     <Navbar bg="light" variant="light">
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Navbar.Brand href="#home">
+        <img
+          src="./global-macspec-logo.svg"
+          width="10"
+          height="10"
+          //className="d-inline-block align-top"
+          alt="global macspec logo"
+        />
+      </Navbar.Brand>
       <Nav className="mr-auto">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#features">Features</Nav.Link>
@@ -16,7 +23,7 @@ const NavBar = () => {
         <Button variant="outline-primary">Search</Button>
       </Form>
     </Navbar>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/Containers/Home/index.js
+++ src/Containers/Home/index.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import NavBar from '../../Components/NavBar'
-
+import React from "react";
+import NavBar from "../../Components/NavBar";
 
 const Home = () => {
   return (
     <div>
       <NavBar />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
