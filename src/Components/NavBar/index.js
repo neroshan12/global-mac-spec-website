c4ae1c124d613c9f833827dd22f23ff703b4c547
Nero <neroshan12@gmail.com>
2020-09-25 17:27:16 +0100

    Update favicon

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,27 +1,18 @@
 import React from "react";
-import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import { Navbar, Nav, Image, FormControl, Button } from "react-bootstrap";
 
 const NavBar = () => {
   return (
     <Navbar bg="light" variant="light">
       <Navbar.Brand href="#home">
-        <img
-          src="./global-macspec-logo.svg"
-          width="10"
-          height="10"
-          //className="d-inline-block align-top"
-          alt="global macspec logo"
-        />
+        <Image src="../../Static/global-macspec-logo.jpg" fluid />
       </Navbar.Brand>
       <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
+        <Nav.Link href="#features">Buy a mac</Nav.Link>
+        <Nav.Link href="#features">Sell a mac</Nav.Link>
+        <Nav.Link href="#pricing">Repairs</Nav.Link>
+        <Nav.Link href="#pricing">Contact Us</Nav.Link>
       </Nav>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-primary">Search</Button>
-      </Form>
     </Navbar>
   );
 };
